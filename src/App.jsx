eaa01641d2c1b2fd5aae6aa3a97373fe152fbf1e
eileen <zwleileen@gmail.com>
2025-01-15 18:06:04 +0800

    feat: added initial css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 // src/App.jsx
 
 const App = () => {
-  return <h1>Hello world!</h1>;
-};
+  
+  return(
+    <>
+  <div className="bookshelfDiv">
+    <h1>My Bookshelf</h1>
+
+    <form className="formDiv">
+      <h2>Add a Book</h2>
+      <p>Title: </p>
+      <p>Author: </p>
+      <button>Add Book</button>
+    </form>
+  </div>
+
+  <div className="bookCardsDiv">
+      <ul className="bookCard">
+      <h2>Fourth Wing</h2>
+      <p>by Rebecca Yarros</p>
+      </ul>
+  </div>
+  </>
+)};
 
 export default App;
